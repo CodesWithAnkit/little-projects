06fa395ae3410be2bb086e9b6de5eed31c90f3a3
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2020-01-28 18:50:55 +0530

    animated form and lazy loading

---
+++ lazy-loading/app.js
@@ -0,0 +1,44 @@
+let options = {
+  threshold: 0.9
+};
+const observer = new IntersectionObserver(imageObserver, options);
+
+let imgs = document.querySelectorAll('img.lazy');
+
+imgs.forEach(img => {
+  observer.observe(img);
+});
+
+function imageObserver(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const img = entry.target;
+      const img_src = img.dataset.src;
+      img.src = img_src;
+    }
+  });
+}
+
+// let options = {
+//     threshold: 1
+//   }
+
+//   const observer = new IntersectionObserver(imageObserver, options);
+
+//   function imageObserver(entries, observer) {
+//     entries.forEach(entry => {
+//       if (entry.isIntersecting) {
+//         const img = entry.target;
+//         const img_src = img.dataset.src;
+//         console.log("Lazy loading ", img);
+//         img.src = img_src;
+//         observer.unobserve(img);
+//       }
+//     })
+//   }
+
+//   let imgs = document.querySelectorAll('img.lazy');
+
+//   imgs.forEach(img => {
+//     observer.observe(img);
+//   })
